97f9becbd8d7ee0b75669762c619ff218685b36a
EhssanBx0 <ehssanbaghban@outlook.com>
2025-08-28 20:53:37 +0100

    add players score counters

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function getComputerChoice(){
     let randNum = Math.floor(Math.random()*3)
-    if (randNum == 0) return "rock"
-    if (randNum == 1) return "paper"
-    if (randNum == 2) return "scissors"
-}
+    if (randNum == 0) return "rock";
+    if (randNum == 1) return "paper";
+    if (randNum == 2) return "scissors";
+};
 
 function getHumanChoice(){
-    return prompt("Select Rock, Paper or Scissors")
-}
+    return prompt("Select Rock, Paper or Scissors");
+};
+
+let humanScore = 0;
+let computerScore = 0;
