56d02a3df39562533bf44c1090b0e15978f8fc19
EhssanBx0 <ehssanbaghban@outlook.com>
2025-08-28 21:40:49 +0100

    create playRound func. to decide round winner

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ function getHumanChoice(){
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanSelection, computerSelection){
+    humanSelection = humanSelection.toLowerCase();
+
+    if (humanSelection === 'rock' && computerSelection === 'scissors' ||
+        humanSelection === 'paper' && computerSelection === 'rock' ||
+        humanSelection === 'scissors' && computerSelection === 'paper'){
+            humanScore++
+            console.log(`You Win! ${humanSelection} beats ${computerSelection}`);
+        }
+
+    if (humanSelection === 'rock' && computerSelection === 'paper' ||
+        humanSelection === 'paper' && computerSelection === 'scissors' ||
+        humanSelection === 'scissors' && computerSelection === 'rock'){
+            computerScore++
+            console.log(`You Lose! ${computerSelection} beats ${humanSelection}`);
+
+        }
+
+    console.log(`Draw! You both chose ${humanSelection}`)
+}
